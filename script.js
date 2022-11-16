59919fff78a5a277f9e2f404b460b3f26af21163
+ <+>
2022-11-16 16:26:45 -0600

    playing with js

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+function sayGreeting() {
+let firstName = prompt("What is your first name?");
+if (firstName.length > 1) {
+alert(`Hello ${firstName}!`);
+}
+}
+
+sayGreeting();
+
+
+function search(event) {
+    event.preventDefault();
+    let name = document.querySelector(#search-city-input);
+}
+
+
+const probabilityOfRain = 0.2;
+const temperatureInC = 26;
+const message =
+  `Hello, ${name}, with a rain chance of 
+${probabilityOfRain * 100}% and a temperature of 
+${temperatureInC}C we recommend that you 
+` +
+  (probabilityOfRain > 0.3 ? "take an umbrella" : "enjoy this rain-free day") +
+  ` ${
+    temperatureInC >= 26
+      ? "and watch out for heatstroke."
+      : " and bask in this fine weather."
+  }`;
+likelyToRain ? "rain boots" : "sun hat";
+console.log(message);
