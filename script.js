d94a4d76e7c5198bbbd19cc3581a04de831184ad
+ <+>
2022-11-17 16:33:32 -0600

    not getting js

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
 function sayGreeting() {
-let firstName = prompt("What is your first name?");
-if (firstName.length > 1) {
-alert(`Hello ${firstName}!`);
-}
+  let firstName = prompt("What is your first name?");
+  if (firstName.length > 1) {
+    alert(`Hello ${firstName}!`);
+  }
 }
 
 sayGreeting();
 
-
-function search(event) {
-    event.preventDefault();
-    let name = document.querySelector(#search-city-input);
+function signUp(event) {
+  event.preventDefault();
+  let input = document.querySelector("#email");
+  console.log(input.value);
 }
-
-
-const probabilityOfRain = 0.2;
-const temperatureInC = 26;
-const message =
-  `Hello, ${name}, with a rain chance of 
-${probabilityOfRain * 100}% and a temperature of 
-${temperatureInC}C we recommend that you 
-` +
-  (probabilityOfRain > 0.3 ? "take an umbrella" : "enjoy this rain-free day") +
-  ` ${
-    temperatureInC >= 26
-      ? "and watch out for heatstroke."
-      : " and bask in this fine weather."
-  }`;
-likelyToRain ? "rain boots" : "sun hat";
-console.log(message);
